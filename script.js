8720a285fc3a69844b4f82cdae2063db133c6bc6
Adam Raider <adamjraider@gmail.com>
2021-07-30 03:24:07 -0400

    render optimizations, new shapes, new collision detections

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const Colors = {
   EMPTY: "white",
 };
 
+const Values = {
+  ACTIVE: 1,
+  EMPTY: 0,
+};
+
 class Piece {
   constructor(positions, color) {
     this.positions = positions;
@@ -24,11 +29,19 @@ class Piece {
   }
 
   rotateRight() {
+    this.positionIndex = this.nextPositionIndex();
+  }
+
+  nextPositionIndex() {
     if (this.positionIndex === this.positions.length - 1) {
-      this.positionIndex = 0;
+      return 0;
     } else {
-      this.positionIndex++;
+      return this.positionIndex + 1;
     }
+  }
+
+  nextRotationCells() {
+    return this.positions[this.nextPositionIndex()];
   }
 }
 
@@ -61,6 +74,93 @@ const Pieces = {
     ],
     "#475F94"
   ),
+  L: new Piece(
+    [
+      [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [2, 1],
+      ],
+      [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+        [0, 2],
+      ],
+      [
+        [0, 0],
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ],
+      [
+        [0, 0],
+        [1, 0],
+        [0, 1],
+        [0, 2],
+      ],
+    ],
+    "#475F94"
+  ),
+  REVERSE_L: new Piece(
+    [
+      [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [2, 0],
+      ],
+      [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [1, 2],
+      ],
+      [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [2, 0],
+      ],
+      [
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ],
+    ],
+    "#475F94"
+  ),
+  ZOID: new Piece(
+    [
+      [
+        [0, 1],
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ],
+      [
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [2, 1],
+      ],
+      [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [1, 1],
+      ],
+      [
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [2, 0],
+      ],
+    ],
+    "#475F94"
+  ),
 };
 
 function makeEmptyBoard(
@@ -68,7 +168,7 @@ function makeEmptyBoard(
   cols,
   defaultValue = {
     color: Colors.EMPTY,
-    value: 0,
+    value: Values.EMPTY,
   }
 ) {
   return [...Array(rows)].map((_) => Array(cols).fill(defaultValue));
@@ -79,12 +179,37 @@ class Tetris {
     this.bindEventListeners();
 
     this.board = makeEmptyBoard(ROWS, COLS);
-    this.currentPiece = Pieces.LINE;
-    this.currentPiecePosition = [0, 8];
+    this.newPiece();
 
-    this.board[10][8] = { value: 1, color: Colors.ACTIVE };
-    this.draw();
-    this.interval = setInterval(this.tick.bind(this), 100);
+    // this.board[10][8] = { value: Values.ACTIVE, color: Colors.ACTIVE };
+    this.board[ROWS - 1].fill({ value: Values.ACTIVE, color: Colors.ACTIVE });
+    this.board[ROWS - 2].fill({ value: Values.ACTIVE, color: Colors.ACTIVE });
+    this.board[ROWS - 3].fill({ value: Values.ACTIVE, color: Colors.ACTIVE });
+    this.board[ROWS - 1][0] = {
+      color: Colors.EMPTY,
+      value: Values.EMPTY,
+    };
+    this.board[ROWS - 2][0] = {
+      color: Colors.EMPTY,
+      value: Values.EMPTY,
+    };
+    this.board[ROWS - 3][0] = {
+      color: Colors.EMPTY,
+      value: Values.EMPTY,
+    };
+    this.previousPos = [];
+    this.initializeDom();
+    this.interval = setInterval(this.tick.bind(this), 50);
+  }
+
+  start() {
+    if (this.interval) return;
+    this.interval = setInterval(this.tick.bind(this), 50);
+  }
+
+  stop() {
+    clearTimeout(this.interval);
+    this.interval = null;
   }
 
   bindEventListeners() {
@@ -100,13 +225,33 @@ class Tetris {
           this.currentPiecePosition[1]--;
           this.draw();
         }
-      } else if (e.key === "r") {
+      } else if (e.key === "g") {
+        if (!this.checkCollision(0, this.currentPiece.nextRotationCells())) {
+        } else if (
+          !this.checkCollision(-1, this.currentPiece.nextRotationCells())
+        ) {
+          this.currentPiecePosition[1]--;
+        } else if (
+          !this.checkCollision(-2, this.currentPiece.nextRotationCells())
+        ) {
+          this.currentPiecePosition[1] = this.currentPiecePosition[1] - 2;
+        } else if (
+          !this.checkCollision(-3, this.currentPiece.nextRotationCells())
+        ) {
+          this.currentPiecePosition[1] = this.currentPiecePosition[1] - 3;
+        } else {
+          // can't rotate?
+          return;
+        }
         this.currentPiece.rotateRight();
+        this.draw();
+      } else if (e.key === "Control") {
+        this.interval ? this.stop() : this.start();
       }
     });
   }
 
-  draw() {
+  initializeDom() {
     gameEl.innerHTML = this.board
       .map((row, rowIndex) => {
         return `<div class="row">${row
@@ -118,6 +263,7 @@ class Tetris {
                 return rowIndex === x + cell[0] && colIndex === y + cell[1];
               })
             ) {
+              this.previousPos.push([rowIndex, colIndex]);
               color = this.currentPiece.color;
             }
 
@@ -128,44 +274,87 @@ class Tetris {
       .join("");
   }
 
+  draw() {
+    this.previousPos.forEach(([x, y]) => {
+      const el = gameEl.children[x].children[y];
+      el.style.backgroundColor = Colors.EMPTY;
+    });
+
+    this.previousPos = [];
+
+    this.currentPiece.getCells().forEach((cell) => {
+      const [x, y] = this.currentPiecePosition;
+      const newX = x + cell[0];
+      const newY = y + cell[1];
+      const el = gameEl.children[newX].children[newY];
+      this.previousPos.push([newX, newY]);
+      el.style.backgroundColor = this.currentPiece.color;
+    });
+  }
+
   tick() {
     this.currentPiecePosition[0]++;
 
     if (this.checkCollision()) {
       this.currentPiecePosition[0]--;
-      this.currentPiece.getCells().forEach((cell) => {
-        this.board[this.currentPiecePosition[0] + cell[0]][
-          this.currentPiecePosition[1] + cell[1]
-        ] = {
-          color: this.currentPiece.color,
-          value: 1,
-        };
-      });
+      this.stop();
+      this.burnPiece();
+      this.previousPos = [];
       this.newPiece();
+      this.resolveRows();
+      this.start();
       return;
     }
 
     this.draw();
   }
 
+  resolveRows() {
+    this.board.forEach((row, rowIndex) => {
+      if (row.every((cell) => cell.value === Values.ACTIVE)) {
+        for (let i = rowIndex - 1; i >= 0; i--) {
+          for (let c = 0; c < COLS; c++) {
+            this.board[i + 1][c] = { ...this.board[i][c] };
+          }
+        }
+        this.initializeDom();
+      }
+    });
+  }
+
+  burnPiece() {
+    this.currentPiece.getCells().forEach((cell) => {
+      this.board[this.currentPiecePosition[0] + cell[0]][
+        this.currentPiecePosition[1] + cell[1]
+      ] = {
+        color: this.currentPiece.color,
+        value: Values.ACTIVE,
+      };
+      const el =
+        gameEl.children[this.currentPiecePosition[0] + cell[0]].children[
+          this.currentPiecePosition[1] + cell[1]
+        ];
+      el.style.backgroundColor = this.currentPiece.color;
+    });
+  }
+
   newPiece() {
-    this.currentPiecePosition = [0, Math.floor(Math.random() * COLS)];
+    this.currentPiecePosition = [
+      0,
+      Math.min(Math.max(1, Math.floor(Math.random() * COLS)), COLS - 4),
+    ];
     this.currentPiece = Pieces[getRandomPiece()];
   }
 
-  checkCollision(offset = 0) {
-    return this.currentPiece.getCells().some((cell) => {
+  checkCollision(offset = 0, cells = this.currentPiece.getCells()) {
+    return cells.some((cell) => {
       const [x, y] = this.currentPiecePosition;
       return (
         typeof this.board[x + cell[0]] === "undefined" ||
         typeof this.board[x + cell[0]][offset + y + cell[1]] === "undefined" ||
-        this.board[x + cell[0]][offset + y + cell[1]].value != 0
+        this.board[x + cell[0]][offset + y + cell[1]].value != Values.EMPTY
       );
     });
-  }
-
-  stop() {
-    clearTimeout(this.interval);
   }
 }
 
@@ -174,4 +363,4 @@ function getRandomPiece() {
   return piecesKeys[Math.floor(Math.random() * piecesKeys.length)];
 }
 
-new Tetris();
+const game = new Tetris();
